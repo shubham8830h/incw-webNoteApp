dda1b682543b9e7aa169e6533187773a5ff8e391
shubham8830h <shubhamhanamane1111@gmail.com>
2023-06-11 08:27:19 +0530

    error resolve

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,12 @@ const app = express();
 const dotenv = require("dotenv");
 const connectToDB = require("./config/database");
 const router = require("./router/route");
+const cors = require("cors");
 
 //middleware
 dotenv.config();
 app.use(express.json());
+app.use(cors());
 
 app.use("/webnotes", router);
 
